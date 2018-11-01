310064d7e1f4eb2d893bfc295b2d9ab84f6fba32
fr3d100 <f.bonnand@gmail.com>
2018-11-01 17:00:50 +0100

    Ajout du jeudis0

---
+++ jeudis0/exo_05.rb
@@ -0,0 +1,28 @@
+#Affiche le texte "On va compter le nombre d'heures de travail à THP"
+puts "On va compter le nombre d'heures de travail à THP"
+
+#Affiche le résultat du calcul 10*5*11 et le concatene dans une chaine de texte
+puts "Travail : #{10 * 5 * 11}"
+
+#Affiche le résultat du calcul 10*5*11*60 et le concatene dans une chaine de texte
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+puts "Et en secondes ?"
+#Affiche le résultat du calcul 10*5*11*60*60
+puts 10 * 5 * 11 * 60 * 60
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+#Affiche le boolean de la proposition "Est ce que 3+2 est inférieur à 5-7 ?"
+puts 3 + 2 < 5 - 7
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
